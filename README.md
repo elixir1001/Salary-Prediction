740deac32255ed232c47975aea4c7711b725b85f
Piyush Gautam <piyush.uni1001@gmail.com>
2023-11-01 20:50:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# StackOverFlow Developer Survey Data Analysis
+
+Link to the dataset: https://insights.stackoverflow.com/survey
+
+Here I am using above dataset to analyze the results of developer survey conducted by StackOverFlow, and create a Machine Learning Model to predict Salary of Software Developers.
+
+1. I cleaned the data with pandas and numpy, code can be found in Jupyter Notebook
+2. I tested 3 Machine Learning Models
+3. I created a Streamlit Webpage with 2 pages:- one to explore the data and other to use the Machine Learning Model to predict salary of software developer given Country, Education, and Experience.
---
+++ predict_page.py
@@ -0,0 +1,125 @@
+<<<<<<< HEAD
+import streamlit as st
+import pickle
+import numpy as np
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+regressor = data["model"]
+le_country = data["le_country"]
+le_education = data["le_education"]
+
+
+
+def show_predict_page():
+    st.title("Software Developer Salary Prediction")
+    st.write("""### We need some information for predicting the salary""")
+
+    countries = {
+        "United States of America",
+        "India",
+        "United Kingdom",
+        "Germany",
+        "Canada",
+        "Brazil",
+        "France",
+        "Spain",
+        "Australia",
+        "Netherlands",
+        "Poland",
+        "Italy",
+        "Russian Federation",
+        "Sweden"
+    }
+
+    education = {
+        "Less than a bachelors",
+        "Bachelors degree",
+        "Masters degree",
+        "Post grad"
+    }
+
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education Level", education)
+
+    experience = st.slider("Years of Experience", 0, 50, 3)
+
+    ok = st.button("Predict Salary")
+    if ok:
+        X = np.array([[country, education, experience]])
+        X[:, 0] = le_country.transform(X[:,0])
+        X[:, 1] = le_education.transform(X[:,1])
+        X = X.astype(float)
+        y_pred = regressor.predict(X)
+
+        salary = regressor.predict(X)
+        st.subheader(f"The estimated Salary is ${salary[0]:.2f}")
+
+    st.write(np.__version__)
+=======
+import streamlit as st
+import pickle
+import numpy as np
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+regressor = data["model"]
+le_country = data["le_country"]
+le_education = data["le_education"]
+
+
+
+def show_predict_page():
+    st.title("Software Developer Salary Prediction")
+    st.write("""### We need some information for predicting the salary""")
+
+    countries = {
+        "United States of America",
+        "India",
+        "United Kingdom",
+        "Germany",
+        "Canada",
+        "Brazil",
+        "France",
+        "Spain",
+        "Australia",
+        "Netherlands",
+        "Poland",
+        "Italy",
+        "Russian Federation",
+        "Sweden"
+    }
+
+    education = {
+        "Less than a bachelors",
+        "Bachelors degree",
+        "Masters degree",
+        "Post grad"
+    }
+
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education Level", education)
+
+    experience = st.slider("Years of Experience", 0, 50, 3)
+
+    ok = st.button("Predict Salary")
+    if ok:
+        X = np.array([[country, education, experience]])
+        X[:, 0] = le_country.transform(X[:,0])
+        X[:, 1] = le_education.transform(X[:,1])
+        X = X.astype(float)
+        y_pred = regressor.predict(X)
+
+        salary = regressor.predict(X)
+        st.subheader(f"The estimated Salary is ${salary[0]:.2f}")
+>>>>>>> b41f596f6209bb1d339f7acab2f414b2e1df9978
